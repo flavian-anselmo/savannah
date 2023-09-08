15ab877ac17c3a304bcf1f266518f07cb58d5af3
flavian-anselmo <anselmoflavian4@gmail.com>
2023-09-08 16:58:30 +0300

    update: setup

---
+++ app/models/models.py
@@ -0,0 +1,5 @@
+from sqlalchemy.orm import relationship
+from app.database.database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+
+
---
+++ app/utils/auth_utils.py
@@ -0,0 +1,18 @@
+from passlib.context import CryptContext
+
+
+password_conxt = CryptContext(schemes=['bcrypt'])
+
+def  get_hashed_password(password:str) -> str:
+    '''
+    takes plain password and returns the hashed version 
+    
+    '''
+    return password_conxt.hash(password)
+
+def verify_password(password:str, hashed_password:str)-> str:
+    '''
+    confirms if the hash and the pswd match 
+    
+    '''
+    return password_conxt.verify(password, hashed_password) 
