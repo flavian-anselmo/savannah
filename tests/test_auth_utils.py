607e0532edd7b92e4b2b449f27405d0feccdf8c9
flavian-anselmo <anselmoflavian4@gmail.com>
2023-09-12 12:13:36 +0300

    update: test auth utils

--- tests/test_auth_utils.py
+++ tests/test_auth_utils.py
@@ -5,16 +5,16 @@ from app.utils.auth_utils import get_hashed_password, verify_password, password_
 
 def test_get_hashed_password_and_verify_func():
     password = '12345678'
-    # verify the hash 
-    hash = get_hashed_password(password=password)
+
+    hash_1 = get_hashed_password(password=password)
     hash_2 = password_conxt.hash(password)
-    assert hash != hash_2
-    assert type(hash) == type('str')
+    assert hash_1 != hash_2
+    assert type(hash_1) == type('str')
 
-    # check if the pswd is verified 
-    verfy = verify_password(password, hash)
-    assert  verfy == True
-
+    verfy_1 = verify_password(password, hash_1)
+    verfy_2 = verify_password(password, hash_2)
+    assert verfy_1 == True
+    assert verfy_2 == True
 
 
 
