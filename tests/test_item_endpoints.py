d0c9981e94ccb436be6e75ba18d942a3321d82d7
flavian-anselmo <anselmoflavian4@gmail.com>
2023-09-16 15:34:49 +0300

    update: added unauth tests

--- tests/test_item_endpoints.py
+++ tests/test_item_endpoints.py
@@ -48,8 +48,16 @@ def test_get_one_item_not_found(authorized_client):
     )
     assert res.json()['detail'] == 'no item found'
     assert res.status_code == status.HTTP_404_NOT_FOUND
-
-
+def test_unauthorized_user_get_all_posts(client):
+    res = client.get(
+        '/items'
+    )
+    print(res.json())
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+def test_unauthorized_get_one_item(client):
+    res = client.get('/items/1')
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
+    
 @pytest.mark.skip()
 def test_update_item(authorized_client, create_a_test_item):
     res = authorized_client.put(
