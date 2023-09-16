fbc554a4182060efe0e23803a99a3333635c66f5
flavian-anselmo <anselmoflavian4@gmail.com>
2023-09-16 20:06:33 +0300

    update: added tests for orders with no item & without auth

--- tests/test_orders_endpoints.py
+++ tests/test_orders_endpoints.py
@@ -3,7 +3,7 @@ from app.schema import schema
 import pytest
 
 
-
+@pytest.mark.skip(reason='avoid sending messages using my trial twilio account')
 def test_make_orders_endpoint(authorized_client,create_multiple_test_items, create_multiple_orders):
     res = authorized_client.post(
         '/orders/2',
@@ -16,6 +16,17 @@ def test_make_orders_endpoint(authorized_client,create_multiple_test_items, crea
     print(res.json())
     assert res.status_code == status.HTTP_201_CREATED
 
+def test_make_orders_to_an_item_that_does_not_exist(authorized_client, create_multiple_test_items):
+    res = authorized_client.post(
+        '/orders/10',
+        json={
+            'customer_id':1,
+            'quantity':1,
+            'item_id':10
+        }
+    )
+    assert res.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+
 
 def test_oders_made_endpoint(authorized_client, create_multiple_orders):
     res = authorized_client.get(
@@ -24,3 +35,8 @@ def test_oders_made_endpoint(authorized_client, create_multiple_orders):
     print(res.json())
     assert res.status_code == status.HTTP_200_OK
 
+def test_get_orders_made_without_auth(client, create_multiple_orders):
+    res = client.get(
+        '/orders'
+    )
+    assert res.status_code == status.HTTP_401_UNAUTHORIZED
